ed047437ca23f7afdb1c0cf0b552855f5c40b2a7
Gustavo Vieira <gvieira.26gomes@gmail.com>
2024-06-04 09:21:16 -0300

    correção tratamento de dados

--- spreadsheets_conn.py
+++ spreadsheets_conn.py
@@ -5,6 +5,9 @@ import datetime
 import os
 import toml
 import json
+import dotenv
+
+dotenv.load_dotenv()
 
 @st.cache_data(show_spinner=False)
 def cria_secrets_file():
@@ -22,11 +25,10 @@ def pegar_planilha(worksheet: str, spreadsheet: str) -> pd.DataFrame:
     except: pass
     try: df.rename(columns={'Reserva': 'Localizador'}, inplace=True)
     except:pass
-    df = df[df['Localizador'].isna() == False]
     return df
 
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data(ttl=300, show_spinner=False)
 def concatenar_planliha_de_custos_faturamento() -> pd.DataFrame:
     # PEGANDO TODAS AS PLANILHAS
     meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ]
@@ -35,46 +37,59 @@ def concatenar_planliha_de_custos_faturamento() -> pd.DataFrame:
         faturamento = pegar_planilha(spreadsheet=os.environ.get('NOME_PLANILHA_FATURAMENTO'), worksheet='Cadastro de Emissoes')
         custo = pegar_planilha(spreadsheet=os.environ.get('NOME_PLANILHA_CUSTO'), worksheet='Fornecedores')
         voltas = pegar_planilha(spreadsheet=os.environ.get('NOME_PLANILHA_VOLTAS'), worksheet='2024')
-        voltas_ = pegar_planilha(spreadsheet=os.environ.get('NOME_PLANILHA_VOLTAS'), worksheet=meses[month_index])
+        voltas_ = pd.DataFrame()
+        for mes in range(0, month_index+1):
+            try: df = pegar_planilha(spreadsheet=os.environ.get('NOME_PLANILHA_VOLTAS'), worksheet=meses[mes]).dropna(subset='Data')
+            except:continue
+            else: voltas_ = pd.concat([voltas_, df], ignore_index=True)
     # EDITANDO FATURAMENTO
     faturamento.rename(columns={'Carimbo de data/hora': 'Data da Emissao', 'Valor total da venda\n': 'Total Venda', 'Quantidade de Passageiros': 'CPFs', 'Taxas De Embarque em REAL ( Dolar e Euro converter para real )': 'Taxas De Embarque', 'Quem Emitiu': 'Emitido por'}, inplace=True)
+    faturamento.dropna(subset='Localizador', inplace=True)
     faturamento = faturamento[['Data da Emissao', 'Cliente', 'Localizador', 'Cia', 'Quantidade de Milhas', 'Taxas De Embarque', 'Total Venda', 'CPFs', 'Emitido por', 'Quem pagou as taxas', 'Titular', 'Login', 'Senha']]
     faturamento.drop_duplicates(subset='Localizador',keep='last', inplace=True)
-    faturamento.loc[:, 'Data da Emissao'] = faturamento['Data da Emissao'].apply(lambda x: datetime.datetime.strptime(str(x), '%d/%m/%Y %H:%M:%S') if isinstance(x, str) else x)
+    faturamento.loc[:, 'Data da Emissao'] = pd.to_datetime(faturamento['Data da Emissao'], errors='coerce', format='%d/%m/%Y %H:%M:%S')
+    faturamento.loc[:, 'Total Venda'] = pd.to_numeric(faturamento['Total Venda'], errors='coerce')
+    faturamento.loc[:, 'CPFs'] = pd.to_numeric(faturamento['CPFs'], errors='coerce')
+    faturamento.fillna({'CPFs': 1}, inplace=True)
+    faturamento.dropna(subset=['Total Venda', 'Data da Emissao', 'Cliente', 'Cia'], inplace=True)
     faturamento.set_index('Localizador', inplace=True)
-    # PEGANDO CUSTO E EDITANDO DF
+    ## PEGANDO CUSTO E EDITANDO DF
     custo.rename(columns={'TotalMilhas+Taxas':'Total Custo'}, inplace=True)
     custo = custo[['Localizador', 'Total Custo']]
-    custo.drop_duplicates(keep='last', inplace=True, subset='Localizador')
+    custo.loc['Total Custo'] = pd.to_numeric(custo['Total Custo'], errors='coerce')
+    custo.dropna(inplace=True)
+    custo.drop_duplicates(subset='Localizador', keep='last', inplace=True)
     custo.set_index('Localizador', inplace=True)
-    # PEGANDO VOLTAS E EDITANDO DF
-    voltas = pd.concat([voltas, voltas_], ignore_index=True)
+    ## PEGANDO VOLTAS E EDITANDO DF
+    voltas = pd.concat([voltas, voltas_])
     voltas.rename(columns={'Data': 'Data da Emissao', 'Venda Realizada para': 'Cliente', 'Custo total':'Total Custo', 'Venda total':'Total Venda'}, inplace=True)
+    voltas.loc[:, 'Data da Emissao'] = pd.to_datetime(voltas['Data da Emissao'], errors='coerce', format='%d/%m/%Y')
+    voltas.loc[:, 'Custo/cpf'] =pd.to_numeric(voltas['Custo/cpf'], errors='coerce')
+    voltas.loc[:, 'CPFs'] =pd.to_numeric(voltas['CPFs'], errors='coerce')
+    voltas.loc[:, 'Valor Venda'] = pd.to_numeric(voltas['Valor Venda'], errors='coerce')
+    voltas.loc[:, 'Total Custo'] = pd.to_numeric(voltas['Total Custo'], errors='coerce')
+    voltas.loc[:, 'Total Venda'] = pd.to_numeric(voltas['Total Venda'], errors='coerce')
+    voltas.dropna(subset=['Custo/cpf',  'CPFs', 'Valor Venda', 'Total Custo', 'Total Venda'],inplace=True)
     voltas = voltas[['Data da Emissao', 'CPFs', 'Cliente', 'Localizador', 'Status', 'Total Custo', 'Total Venda', 'Emitido por']]
-    voltas = voltas[voltas['CPFs'] != '-']
-    voltas.dropna(subset='Data da Emissao', inplace=True)
-    voltas.loc[:, 'Data da Emissao'] = voltas['Data da Emissao'].apply(lambda x: datetime.datetime.strptime(str(x), '%d/%m/%Y'))
     voltas['Tipo'] = ['Volta' for x in range(len(voltas.index))]
-    voltas.fillna({'Status': 'A Cancelar'}, inplace=True)
+    voltas.fillna({'Status': 'A Cancelar', 'Localizador': '-'}, inplace=True)
     voltas['num emit'] = voltas['CPFs'].copy()
-    # UNINDO OS DATAFRAMES
+    voltas.sort_values(by='Data da Emissao', ignore_index=True)
+
+    ## UNINDO OS DATAFRAMES
     df_ = pd.merge(faturamento, custo, left_index=True, right_index=True, how='inner')
     df_.reset_index(inplace=True)
     df_['Tipo'] = ['Emissao' for x in range(len(df_.index))]
     df_['num emit'] = [1 for x in range(len(df_.index))]
     df = pd.concat([df_, voltas], axis=0, ignore_index=True)
-    # TRATANDO DF FINAL
-    df.fillna({'Cliente': 'Não identificado', 'Cia': 'Não identificada', 'Quantidade de Milhas': 0, 'Taxas De Embarque': 0, 'Total Venda': 0, 'CPFs': 1, 'Milheiro Venda': 0, 'Milheiro Compra': 0, 'Custo Parcial': 0, 'Total Custo': 0, 'Status': '-'}, inplace=True)
-    df.loc[:, 'Total Venda'] = pd.to_numeric(df['Total Venda'].apply(lambda x: str(x).replace('R$ ', '').replace(',', '.')), errors='coerce')
-    df.loc[:, 'Total Custo'] = pd.to_numeric(df['Total Custo'].apply(lambda x: str(x).replace('R$ ', '').replace(',', '.')), errors='coerce')
-    df['Quantidade de Milhas'] = pd.to_numeric(df['Quantidade de Milhas'], errors='coerce')
+    ## TRATANDO DF FINAL
+    df.fillna({'Cliente': 'Volta Cancelada', 'Cia': 'Volta Cancelada', 'Quantidade de Milhas': 0, 'Taxas De Embarque': 0, 'Total Venda': 0, 'Milheiro Venda': 0, 'Milheiro Compra': 0, 'Custo Parcial': 0, 'Total Custo': 0, 'Status': '-', 'Quem pagou as taxas': '-', 'Titular': '-', 'Login':'-', 'Senha': '-'}, inplace=True)
     df['Lucro'] = df['Total Venda'] - df['Total Custo']
     df.loc[:, 'Cia'] = df['Cia'].apply(lambda x: str(x).title().strip())
     df.loc[:, 'Cliente'] = df['Cliente'].apply(lambda x: str(x).title().strip())
-    df.loc[:, 'CPFs'] = pd.to_numeric(df['CPFs'], errors='coerce').dropna().apply(lambda x: int(x))
     return df
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data(ttl=600, show_spinner=False)
 def pesquisar_logins():
     df_options = {'usernames':{}}
     conn = st.connection('emissionsconnect', type=GSheetsConnection)
